8abd0abfa14069a711f0bbd44feac857ce4cb462
Yotta-Alien <161250059@smail.nju.edu.cn>
2017-12-06 09:47:22 +0800

    update 12.6 9:47

--- src/client/Presentation/AccountantUI/BillFill/ReceiveUI.java
+++ src/client/Presentation/AccountantUI/BillFill/ReceiveUI.java
@@ -179,6 +179,7 @@ public class ReceiveUI extends Application {
                                 e.printStackTrace();
                             }
 
+                            
                         });
                     }
                 }
