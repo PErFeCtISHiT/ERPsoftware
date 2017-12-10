09081f099ad2d18d535107098245f33ce2f08c49
liuyitong <849798320@qq.com>
2017-12-10 12:21:10 +0800

    @date: 12:21 2017/12/10

--- src/server/Start/start.java
+++ src/server/Start/start.java
@@ -3,13 +3,18 @@ package server.Start;
 import server.RMIservice.RemoteHelper;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class start {
 
     public static void main(String[] args) throws IOException {
-        //String cmd = "sqlplus pis/liuyitong@sid @g://123.txt";
-        //Process process = Runtime.getRuntime().exec(cmd);
-        new RemoteHelper();
+        File file = new File("./src/shared/sqlplusroot");
+        System.out.println(file.getAbsolutePath());
+        String cmd = "sqlplus pis/liuyitong@172.26.71.52:1521/pis.exercise @";
+        cmd += file.getAbsolutePath();
+        System.out.print(cmd);
+        Runtime.getRuntime().exec(cmd);
+        //new RemoteHelper();
     }
 }
--- src/source/sqlplusroot.sql
+++ src/source/sqlplusroot.sql
@@ -2,6 +2,6 @@ set linesize 200
 set term off verify off feedback off pagesize 999
 set markup html on entmap ON spool on preformat off
 spool g://test_tables.xls
-@src/shared/goodsToExcel
+@g://1.txt
 spool off
 exit
