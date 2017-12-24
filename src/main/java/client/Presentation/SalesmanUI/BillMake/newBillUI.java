5969dc91d8af186653ff2b54f166023c5fcc5283
Ferry <yutian110@126.com>
2017-12-24 21:04:53 +0800

    Merge branch 'master' of https://github.com/PErFeCtISHiT/ERPsoftware
    complete most !

--- src/main/java/client/Presentation/SalesmanUI/BillMake/newBillUI.java
+++ src/main/java/client/Presentation/SalesmanUI/BillMake/newBillUI.java
@@ -1699,6 +1699,8 @@ public class newBillUI {
         Label operaterLabel=new Label("操作员：");
         TextField operater=new TextField();
 
+        
+
         gridPane.add(operaterLabel,0,3);
         gridPane.add(operater,1,3);
 
